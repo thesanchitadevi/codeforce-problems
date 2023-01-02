996fd5eefc4e6345ee506f56d582c1b6d64abdab
Sanchita Devi <thesanchitadevi@gmail.com>
2023-01-02 21:11:47 +0600

    Add files via upload

---
+++ 1676A.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        string s;
+        cin >> s;
+        int a = s[0]-'0', b = s[1]-'0', c = s[2]-'0';
+        int x = s[3]-'0', y = s[4]-'0', z = s[5]-'0';
+        if(a+b+c == x+y+z){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+        
+    }
+}
