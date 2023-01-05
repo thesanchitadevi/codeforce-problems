b2a30387c021f36cb55fc554addd180bc7bb796c
Sanchita Devi <thesanchitadevi@gmail.com>
2023-01-05 21:15:43 +0600

    Add files via upload

---
+++ 1768A.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    while(n--)
+    {
+        int x;
+        cin >> x;
+        cout << x-1 << endl;
+    }
+    return 0;
+}
